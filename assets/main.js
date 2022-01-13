4bf6bbb6019efc5ec8d9f0385b8173f42c98da91
dungdangcap <dungvipvai205@gmail.com>
2022-01-13 18:44:04 +0700

    second commit

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $(".suggest-product, .new-product").slick({
+        slidesToShow: 4,
+        // slidesToScroll: 2,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false,
+                outline: false,
+              }
+            }
+          ],
+    });
+  });
